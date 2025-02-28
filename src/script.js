561409be41a23bd7dfd6a7819812ac19309e5469
Ayrton Yamashita <ayrtonyamashita@gmail.com>
2025-02-27 22:25:07 -0400

    Script min 1.0.2

--- src/script.js
+++ src/script.js
@@ -3,43 +3,53 @@ const fatura = document.querySelector("#nro_fatura");
 const valor_fatura = document.querySelector("#vlr");
 console.log("Script carregado!")
 
-async function processarDocumentos() {
-  const localErrors = localStorage.getItem("log");
-  if (localErrors) {
-    localStorage.removeItem("log");
-  }
-  for (const [index, doc] of documentos.entries()) {
-    const chave = Object.keys(doc)[0];
-    const valor = doc[chave];
-    // document.getElementById("1").value = chave;
-    document.getElementById("2").value = valor;
-    document.getElementById("3").click();
-    await new Promise(resolve => setTimeout(resolve, 500));
-    console.log(chave, valor)
+// async function processarDocumentos() {
+//   const localErrors = localStorage.getItem("log");
+//   if (localErrors) {
+//     localStorage.removeItem("log");
+//   }
+//   for (const [index, doc] of documentos.entries()) {
+//     const chave = Object.keys(doc)[0];
+//     const valor = doc[chave];
+//     // document.getElementById("1").value = chave;
+//     document.getElementById("2").value = valor;
+//     document.getElementById("3").click();
+//     await new Promise(resolve => setTimeout(resolve, 500));
+//     console.log(chave, valor)
 
-    const errorDiv = document.getElementById("errormsg");
-    if (errorDiv && errorDiv.innerText.trim() !== "") {
-      const mensagemErro = errorDiv.textContent;;
+//     const errorDiv = document.getElementById("errormsg");
+//     if (errorDiv && errorDiv.innerText.trim() !== "") {
+//       const mensagemErro = errorDiv.textContent;;
 
-      const errosAnteriores = JSON.parse(localStorage.getItem("log")) || [];
-      errosAnteriores.push({ documento: doc, erro: mensagemErro });
-      localStorage.setItem("log", JSON.stringify(errosAnteriores));
-      document.getElementById("0").click();
-      await new Promise(resolve => setTimeout(resolve, 500));
-    }
-    if (index === documentos.length - 1) {
-      const erros = JSON.stringify(localStorage.getItem("log"));
-      const blob = new Blob([localStorage.getItem("log")], { type: 'application/json' });
-      const url = URL.createObjectURL(blob);
-      const a = document.createElement("a")
-      a.href = url;
-      document.body.appendChild(a)
-      a.download = `relatorio_erros.txt`
-      a.click()
-      document.body.removeChild(a)
-      URL.revokeObjectURL(url)
+//       const errosAnteriores = JSON.parse(localStorage.getItem("log")) || [];
+//       errosAnteriores.push({ documento: doc, erro: mensagemErro });
+//       localStorage.setItem("log", JSON.stringify(errosAnteriores));
+//       document.getElementById("0").click();
+//       await new Promise(resolve => setTimeout(resolve, 500));
+//     }
+//     if (index === documentos.length - 1) {
+//       const erros = JSON.stringify(localStorage.getItem("log"));
+//       const blob = new Blob([localStorage.getItem("log")], { type: 'application/json' });
+//       const url = URL.createObjectURL(blob);
+//       const a = document.createElement("a")
+//       a.href = url;
+//       document.body.appendChild(a)
+//       a.download = `relatorio_erros.txt`
+//       a.click()
+//       document.body.removeChild(a)
+//       URL.revokeObjectURL(url)
 
-    }
-  }
+//     }
+//   }
+// }
+// processarDocumentos();
+
+function teste() {
+  console.log("Função executada com sucesso!")
 }
-processarDocumentos();
+
+function teste2(texto) {
+  console.log(`Função executada com sucesso! ${texto}`)
+}
+
+
